39c187e39d8ea0b0626101c91df7ed26673d55f2
BasiratHamayoon <basirathumayun@gmail.com>
2026-05-23 12:04:35 +0500

    delete all revious files

--- src/components/theme/ThemeProvider.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/theme/ThemeProvider.jsx
-"use client";
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function ThemeProvider({ children, ...props }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
--- src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-// src/lib/axios.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-});
-
-export default api;
